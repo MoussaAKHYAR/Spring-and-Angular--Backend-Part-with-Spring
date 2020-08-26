f9c5c0c4899acacce80e740498fb1e8493df1eb1
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-08-26 01:33:45 +0000

    gestion d'api Spring

--- src/main/java/sn/isi/informatique/dao/ProduitRepository.java
+++ src/main/java/sn/isi/informatique/dao/ProduitRepository.java
@@ -1,7 +1,19 @@
 package sn.isi.informatique.dao;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import sn.isi.informatique.entities.Produit;
 
+import java.util.List;
+
+@RepositoryRestResource
 public interface ProduitRepository extends JpaRepository<Produit,Long> {
+    @RestResource(path = "/byDesignation")
+    public List<Produit> findByDesignationContains(@Param("mc") String des);
+    @RestResource(path = "/byDesignationPage")
+    public List<Produit> findByDesignationContains(@Param("mc") String des, Pageable pageable);
+
 }
